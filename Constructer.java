dfae2330fae45ab8be7d09418605632ad9980755
SonaliChoudhary239 <169046206+SonaliChoudhary239@users.noreply.github.com>
2024-08-27 23:10:47 +0530

    Add files via upload

---
+++ Constructer.java
@@ -0,0 +1,14 @@
+ class Dog {
+    Dog()
+    {
+        System.out.println("A new dog is created !");
+    }    
+}
+  class  Driver5 
+{
+    
+    public static void main(String[] args) {
+        //creating an object of dog class
+        Dog myDog=new Dog();//A new dog is created !
+    }
+}
